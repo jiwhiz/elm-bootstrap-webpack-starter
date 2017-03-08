8f5f439c086ec49b43e62c77ee90f133e03bce4e
Yuan Ji <yuan.ji@jiwhiz.com>
2017-03-08 00:09:53 -0700

    add route and navbar

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -3,10 +3,14 @@ module Main exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
+import Html.Events exposing (onWithOptions)
+import Navigation exposing (Location)
 import Bootstrap.Button as Button
 import Bootstrap.CDN as CDN
 import Bootstrap.Grid as Grid
 import Bootstrap.Grid.Col as Col
+import Bootstrap.Navbar exposing (State)
+import Routes exposing (..)
 
 
 -- component import example
@@ -14,68 +18,119 @@ import Bootstrap.Grid.Col as Col
 import Components.Hello exposing (hello)
 
 
--- APP
-
-
-main : Program Never Int Msg
-main =
-    Html.beginnerProgram { model = model, view = view, update = update }
-
-
-
--- MODEL
-
-
-type alias Model =
-    Int
-
-
-model : number
-model =
-    0
-
-
-
--- UPDATE
-
-
 type Msg
-    = NoOp
+    = ChangeLocation String
+    | OnLocationChange Location
+    | NavbarMsg State
     | Increment
 
 
-update : Msg -> Model -> Model
+type alias Model =
+    { route : Routes.Route
+    , navbarState : Bootstrap.Navbar.State
+    , count : Int
+    }
+
+
+initialModel : Routes.Route -> Bootstrap.Navbar.State -> Model
+initialModel route state =
+    { route = route
+    , navbarState = state
+    , count = 0
+    }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            model
+        ChangeLocation path ->
+            ( model, Navigation.newUrl path )
+
+        OnLocationChange location ->
+            let
+                newRoute =
+                    Routes.parseLocation location
+            in
+                ( { model | route = newRoute }, Cmd.none )
+
+        NavbarMsg state ->
+            ( { model | navbarState = state }, Cmd.none )
 
         Increment ->
-            model + 1
+            ( { model | count = model.count + 1 }, Cmd.none )
 
 
 view : Model -> Html Msg
 view model =
-    Grid.container [ style [ ( "margin-top", "30px" ), ( "text-align", "center" ) ] ]
+    Grid.container
+        [ style [ ( "margin-top", "30px" ), ( "text-align", "center" ) ]
+        ]
         [ CDN.stylesheet
         , Grid.row []
             [ Grid.col [ Col.xs12 ]
-                [ div [ class "jumbotron" ]
-                    [ img [ src "static/img/elm.jpg", style styles.img ] []
-                      -- inline CSS (via var)
-                    , hello model
-                      -- ext 'hello' component (takes 'model' as arg)
-                    , p [] [ text ("Elm Bootstrap Webpack Starter") ]
-                    , Button.button [ Button.primary, Button.large, Button.attrs [ onClick Increment ] ]
-                        [ -- click handler
-                          span [ class "glyphicon glyphicon-star" ] []
-                          -- glyphicon
-                        , span [] [ text "FTW!" ]
-                        ]
+                [ div []
+                    [ nav model ChangeLocation
+                    , page model
                     ]
                 ]
             ]
         ]
+
+
+page : Model -> Html Msg
+page model =
+    case model.route of
+        HomeRoute ->
+            div [ class "jumbotron" ]
+                [ img [ src "static/img/elm.jpg", style styles.img ] []
+                , hello model.count
+                , p [] [ text ("Elm Bootstrap Webpack Starter") ]
+                , Button.button
+                    [ Button.primary
+                    , Button.large
+                    , Button.attrs [ onClick Increment ]
+                    ]
+                    [ span [ class "glyphicon glyphicon-star" ] []
+                    , span [] [ text "FTW!" ]
+                    ]
+                ]
+
+        AboutRoute ->
+            text "About"
+
+        NotFoundRoute ->
+            text "Not Found"
+
+
+
+-- PROGRAM
+
+
+init : Navigation.Location -> ( Model, Cmd Msg )
+init location =
+    let
+        currentRoute =
+            Routes.parseLocation location
+
+        ( navbarModel, navbarCmd ) =
+            initNavbar NavbarMsg
+    in
+        ( initialModel currentRoute navbarModel, navbarCmd )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main : Program Never Model Msg
+main =
+    Navigation.program OnLocationChange
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
@@ -89,3 +144,57 @@ styles =
         , ( "border", "4px solid #337AB7" )
         ]
     }
+
+
+
+-- NAVBAR
+
+
+initNavbar : (State -> msg) -> ( State, Cmd msg )
+initNavbar tagger =
+    let
+        ( navbarState, navbarCmd ) =
+            Bootstrap.Navbar.initialState tagger
+    in
+        ( navbarState, navbarCmd )
+
+
+routeToItemLink : (String -> msg) -> Routes.Route -> Routes.Route -> String -> Bootstrap.Navbar.Item msg
+routeToItemLink tagger curRoute route title =
+    let
+        path =
+            Routes.encode route
+
+        itemLink =
+            if curRoute == route then
+                Bootstrap.Navbar.itemLinkActive
+            else
+                Bootstrap.Navbar.itemLink
+    in
+        itemLink
+            [ href path
+            , attribute "data-navigate" path
+            , Routes.catchNavigationClicks (tagger path)
+            ]
+            [ text title ]
+
+
+nav : Model -> (String -> Msg) -> Html Msg
+nav model changeLocationTagger =
+    Bootstrap.Navbar.config NavbarMsg
+        |> Bootstrap.Navbar.withAnimation
+        |> Bootstrap.Navbar.brand
+            [ href "/" ]
+            [ img
+                [ src "static/img/elm-bootstrap.svg"
+                , class "d-inline-block align-top"
+                , style [ ( "width", "30px" ) ]
+                ]
+                []
+            , text " Project"
+            ]
+        |> Bootstrap.Navbar.items
+            [ routeToItemLink changeLocationTagger model.route Routes.HomeRoute " Home"
+            , routeToItemLink changeLocationTagger model.route Routes.AboutRoute " About"
+            ]
+        |> Bootstrap.Navbar.view model.navbarState
---
+++ src/elm/Routes.elm
@@ -0,0 +1,84 @@
+module Routes exposing (..)
+
+import UrlParser exposing (Parser, (</>), int, oneOf, s)
+import Navigation exposing (Location)
+import Html.Attributes exposing (href, attribute)
+import Html exposing (Html, Attribute, a)
+import Html.Events exposing (onWithOptions)
+import Json.Decode as Json
+
+
+type Route
+    = HomeRoute
+    | AboutRoute
+    | NotFoundRoute
+
+
+routeParser : Parser (Route -> a) a
+routeParser =
+    UrlParser.oneOf
+        [ UrlParser.map HomeRoute (UrlParser.s "")
+        , UrlParser.map AboutRoute (UrlParser.s "about")
+        ]
+
+
+parseLocation : Navigation.Location -> Route
+parseLocation location =
+    case (UrlParser.parsePath routeParser location) of
+        Just route ->
+            route
+
+        Nothing ->
+            NotFoundRoute
+
+
+encode : Route -> String
+encode route =
+    case route of
+        HomeRoute ->
+            "/"
+
+        AboutRoute ->
+            "/about"
+
+        NotFoundRoute ->
+            "/"
+
+
+navigate : Route -> Cmd msg
+navigate route =
+    Navigation.newUrl (encode route)
+
+
+linkTo : Route -> List (Attribute msg) -> List (Html msg) -> Html msg
+linkTo route attrs content =
+    a ((linkAttrs route) ++ attrs) content
+
+
+linkAttrs : Route -> List (Attribute msg)
+linkAttrs route =
+    let
+        path =
+            encode route
+    in
+        [ href path
+        , attribute "data-navigate" path
+        ]
+
+
+catchNavigationClicks : msg -> Attribute msg
+catchNavigationClicks message =
+    onWithOptions "click"
+        { stopPropagation = True
+        , preventDefault = True
+        }
+        (Json.succeed message)
+
+
+pathDecoder : Json.Decoder String
+pathDecoder =
+    Json.oneOf
+        [ Json.at [ "data-navigate" ] Json.string
+        , Json.at [ "parentElement" ] (Json.lazy (\_ -> pathDecoder))
+        , Json.fail "no path found for click"
+        ]
